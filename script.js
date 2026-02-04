31da646a59bb0db15a0233aaa74f7db7311a85ad
kaderaid08 <karinar97@nycstudents.net>
2026-02-04 10:35:00 -0500

    huzzah?

--- script.js
+++ script.js
@@ -4,20 +4,31 @@ const listcontent = document.querySelector("#list");
 const input = document.querySelector("#input");
 const inText = document.querySelector("#taskin");
 const submitButton = document.querySelector("#submit");
+const inTime = document.querySelector("#timein");
+const total = document.querySelector("#total");
+const taskDisplay = document.querySelector("#taskDisplay");
+const timeDisplay = document.querySelector("#timeDisplay");
+
 
 let tasknumber=0;
 
-const addTask = () =>{
+const addTask = () =>{    
+    const timeValue = inTime.value;
+    const totalTime= timeValue + inTime
     const textValue = inText.value;
+
     tasknumber+=1;
-    listcontent.innerHTML += `<br><input type="checkbox" id="task${tasknumber}" name="task${tasknumber}" ><label for="task${tasknumber}"> ${textValue}</label>`;
+    listcontent.innerHTML += `<br><input type="checkbox" id="task${tasknumber}" name="task${tasknumber}" ><label for="task${tasknumber}"> ${textValue}   _____________________   ${timeValue}</label>`;
     inText.value="";
+    inTime.value="";
+    taskDisplay.innerHTML=`<h4 style="font-size:30px; text-align:center;">Tasks left:${tasknumber}</h4>`;
+    timeDisplay.innerHTML= `<h4 style="font-size:30px; text-align:center;">Time left:${totalTime}</h4>`;
 }
 submitButton.addEventListener("click", addTask);
 
-const crossOut = () =>{
-    if (inText.checked==True){
-        console.log("it worked");
-    }
-}
 
+const crossOut = () =>{
+        console.log("it worked");
+        listcontent.classList.add("crossed");
+}
+listcontent.addEventListener("click", crossOut);
